71147cc5f674378f43c8c72c1d29916acd646fad
GeonTae Park <rssr96@naver.com>
2022-12-06 16:08:28 +0900

    구구단 리팩토링

--- src/TimesTable/Main.java
+++ src/TimesTable/Main.java
@@ -1,18 +1,18 @@
 package TimesTable;
 
 public class Main {
-
-    public static int[] caculate(int times) {
-        int[] result = new int[9];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = times * (i + 1);
-        }
-        return result;
-    }
-
-    public static void print(int[] result) {
-        for (int i = 0; i < result.length; i++) {
-            System.out.println(result[i]);
-        }
-    }
+//
+//    public static int[] caculate(int times) {
+//        int[] result = new int[9];
+//        for (int i = 0; i < result.length; i++) {
+//            result[i] = times * (i + 1);
+//        }
+//        return result;
+//    }
+//
+//    public static void print(int[] result) {
+//        for (int i = 0; i < result.length; i++) {
+//            System.out.println(result[i]);
+//        }
+//    }
 }
---
+++ src/TimesTable/TimeTableMain.java
@@ -0,0 +1,29 @@
+package TimesTable;
+
+import java.util.Scanner;
+
+public class TimeTableMain {
+    public static void main(String[] args) {
+
+        System.out.println("출력하실 구구단의 값을 입력하세요 ");
+        System.out.println("Ex) 8,7 입력시 2 * 1 ... 2 * 7, 3 * 1 ... 3 * 7, ... , 8 * 1 ... 8 * 7 까지 구현됩니다.");
+        Scanner scanner = new Scanner(System.in);
+        String inputValue = scanner.nextLine();
+        timeTable(inputValue);
+
+    }
+
+    public static void timeTable(String inputValue) {
+        String[] splitedValue = inputValue.split(",");
+
+        int first = Integer.parseInt(splitedValue[0]);
+        int second = Integer.parseInt(splitedValue[1]);
+
+        for (int i = 2; i <= first; i++) {
+            for (int j = 1; j <= second; j++) {
+                System.out.println(i + " * " + j + " = " + i * j);
+            }
+
+        }
+    }
+}
