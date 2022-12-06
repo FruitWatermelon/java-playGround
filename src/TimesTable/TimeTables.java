7eedf89634f4308b7c2cc6b79542960173121467
GeonTae Park <rssr96@naver.com>
2022-12-06 15:25:45 +0900

    6,7 단 반복문

--- src/TimesTable/TimeTables.java
+++ src/TimesTable/TimeTables.java
@@ -27,39 +27,50 @@ public class TimeTables {
 //        System.out.println(3 * 9);
 
         //4단
-        int result = 4 * 1;
-        System.out.println(result);
-        result = 4 * 2;
-        System.out.println(result);
-        result = 4 * 3;
-        System.out.println(result);
-        result = 4 * 4;
-        System.out.println(result);
-        result = 4 * 5;
-        System.out.println(result);
-        result = 4 * 6;
-        System.out.println(result);
-        result = 4 * 7;
-        System.out.println(result);
-        result = 4 * 8;
-        System.out.println(result);
-        result = 4 * 9;
-        System.out.println(result);
+//        int result = 4 * 1;
+//        System.out.println(result);
+//        result = 4 * 2;
+//        System.out.println(result);
+//        result = 4 * 3;
+//        System.out.println(result);
+//        result = 4 * 4;
+//        System.out.println(result);
+//        result = 4 * 5;
+//        System.out.println(result);
+//        result = 4 * 6;
+//        System.out.println(result);
+//        result = 4 * 7;
+//        System.out.println(result);
+//        result = 4 * 8;
+//        System.out.println(result);
+//        result = 4 * 9;
+//        System.out.println(result);
 
-        System.out.println("출력하고 싶은 단은? :");
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
+        // 5단
+//        System.out.println("출력하고 싶은 단은? :");
+//        Scanner scanner = new Scanner(System.in);
+//        int number = scanner.nextInt();
+//
+//        System.out.println(number * 1);
+//        System.out.println(number * 2);
+//        System.out.println(number * 3);
+//        System.out.println(number * 4);
+//        System.out.println(number * 5);
+//        System.out.println(number * 6);
+//        System.out.println(number * 7);
+//        System.out.println(number * 8);
+//        System.out.println(number * 9);
 
-        System.out.println(number * 1);
-        System.out.println(number * 2);
-        System.out.println(number * 3);
-        System.out.println(number * 4);
-        System.out.println(number * 5);
-        System.out.println(number * 6);
-        System.out.println(number * 7);
-        System.out.println(number * 8);
-        System.out.println(number * 9);
+        // 6단
+        int i = 1;
+        while(i<10){
+            System.out.println(6 * i);
+            i = i+1;
+        }
 
-
+        // 7단
+        for(int j=1; j<10; j++){
+            System.out.println(7 * j);
+        }
     }
 }
