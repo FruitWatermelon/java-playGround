76e7d9286bd4d86eb1138032ac03e2a4b4bf5d61
GeonTae Park <rssr96@naver.com>
2022-12-06 15:41:14 +0900

    class를 통한 재구현

--- src/TimesTable/TimeTables.java
+++ src/TimesTable/TimeTables.java
@@ -3,24 +3,11 @@ package TimesTable;
 import java.util.Scanner;
 
 public class TimeTables {
-    public static int[] caculate(int times) {
-        int[] result = new int[9];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = times * (i + 1);
-        }
-        return result;
-    }
-
-    public static void print(int[] result) {
-        for (int i = 0; i < result.length; i++) {
-            System.out.println(result[i]);
-        }
-    }
 
     public static void main(String[] args) {
         for (int i = 2; i < 10; i++) {
-            int[] result = caculate(i);
-            print(result);
+            int[] result = Main.caculate(i);
+            Main.print(result);
         }
 
         // 2단
